c55e6d3e17ef792fc3810afb641bc65a8bf2fa2c
Alexey Kachura <alexonthework@gmail.com>
2023-11-16 19:17:33 +0100

    Create Project

---
+++ WaterTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WaterTracker
+//
+//  Created by Alexey Kachura on 16.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by Alexey Kachura on 16.11.23.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
